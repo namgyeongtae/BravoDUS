5a2034bba46e2cd91962a5a28499fb9dd064dd8d
namgyeongtae <gotsuebel@gmail.com>
2025-08-27 14:45:26 +0900

    Add Build Action SelectionSO
    건물마다 클릭 시 선택할 Action 버튼의 정보를 담는 SO

---
+++ Assets/Scripts/ScriptableObjects/BuildingSelectionSO.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum BuildingType
+{
+    None,
+    Center,
+    Hospital,
+    PoliceStation,
+    FireStation,
+    ConvenienceStore
+}
+
+public enum BuildingActionType
+{
+    None,
+    Info,
+    Upgrade
+}
+
+[Serializable]
+public class BuildingAction
+{
+    public BuildingActionType actionType;
+    public string actionName;
+    public Sprite icon;
+}
+
+[System.Serializable]
+public class BuildingActionSet
+{
+    public BuildingType buildingType;
+    public List<BuildingAction> availableActions;
+}
+
+[CreateAssetMenu(fileName = "BuildingSelectionSO", menuName = "ScriptableObjects/BuildingSelectionSO")]
+public class BuildingSelectionSO : ScriptableObject
+{
+    public List<BuildingActionSet> actionSets;
+}
