79a7ba0fa285984bacb919b1d12058803f67926c
namgyeongtae <gotsuebel@gmail.com>
2025-08-27 14:47:00 +0900

    Add UIManager to Managers as Singleton

--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -4,10 +4,11 @@ public class Managers
 {
     private static CommodityManager _commodity = new CommodityManager();
     private static ResourceManager _resource = new ResourceManager();
+    private static UIManager _ui = new UIManager();
 
     public static CommodityManager Commodity => _commodity;
     public static ResourceManager Resource => _resource;
-
+    public static UIManager UI => _ui;
     public void Init()
     {
         _commodity.Init();
