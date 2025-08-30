da030a652d43e43af3097228a6f128f0b0a5d4d9
namgyeongtae <gotsuebel@gmail.com>
2025-08-30 17:45:03 +0900

    Add Build UI Scripts

---
+++ Assets/Scripts/UI/UIBuildButtonGroup.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using UnityEngine;
+
+public class UIBuildButtonGroup : CanvasPanel
+{
+    [Bind("Build")] private UIButton _buildButton;
+    [Bind("Cancel")] private UIButton _cancelButton;
+
+    private GameObject _selectedBuilding = null;
+
+    protected override void Initialize()
+    {
+        _buildButton.BindEvent(OnClickBuildButton, ClickType.Up);
+        _cancelButton.BindEvent(OnClickCancelButton, ClickType.Up);
+    }
+
+    public override void SetPanelInfo(object Info)
+    {
+        base.SetPanelInfo(Info);
+
+        _selectedBuilding = (GameObject)Info;
+
+        AdjustPosition();
+    }
+
+    void Update()
+    {
+        if (_selectedBuilding != null)
+        {
+            AdjustPosition();
+        }
+    }
+
+    public override void Open()
+    {
+        base.Open();
+        StartCoroutine(AnimateOpen());
+    }
+
+    public override void Close()
+    {
+        StartCoroutine(AnimateClose());
+    }
+
+    private void OnClickBuildButton()
+    {
+        Debug.Log("Build Start");
+        Close();
+    }
+
+    private void OnClickCancelButton()
+    {
+        Close();
+    }
+
+    private void AdjustPosition()
+    {
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(_selectedBuilding.transform.position + Vector3.up * 5f);
+
+        Rect.position = screenPos;
+    }
+
+    private IEnumerator AnimateOpen()
+    {
+        float duration = 0.5f;
+
+        float time = 0f;
+
+        // 크기가 잠깐 커졌다 쏙 줄어드는 연출
+        while (time < duration)
+        {
+            float t = time / duration;
+            float easedT = UIUtils.EaseInOutQuad(t);
+            
+            // 처음에는 크기가 커졌다가 (1.2배), 그 다음에 원래 크기로 돌아옴
+            float scale = (easedT < 0.5f) ? Mathf.Lerp(0f, 1.2f, easedT * 2f) : Mathf.Lerp(1.2f, 1f, (easedT - 0.5f) * 2f);
+            
+            _buildButton.GetComponent<RectTransform>().localScale = Vector3.one * scale;
+            _cancelButton.GetComponent<RectTransform>().localScale = Vector3.one * scale;
+
+            time += Time.deltaTime;
+            yield return null;
+        }
+    }
+    private IEnumerator AnimateClose()
+    {
+        float duration = 0.5f;
+
+        float time = 0f;
+
+        while (time < duration)
+        {
+            float t = time / duration;
+            float easedT = UIUtils.EaseInOutQuad(t);
+
+            float scale = (easedT < 0.5f) ? Mathf.Lerp(1f, 1.2f, easedT * 2f) : Mathf.Lerp(1.2f, 0f, (easedT - 0.5f) * 2f);
+
+            _buildButton.GetComponent<RectTransform>().localScale = Vector3.one * scale;
+            _cancelButton.GetComponent<RectTransform>().localScale = Vector3.one * scale;
+
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        base.Close();
+    }
+}
