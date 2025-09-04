8c19ef965bad079f2968010ccf186b3f5cb756e7
namgyeongtae <gotsuebel@gmail.com>
2025-09-04 15:05:18 +0900

    Move UI Scripts to folder

---
+++ Assets/Scripts/UI/Normal/UIEventDisplay.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIEventDisplay : CanvasPanel
+{
+    [Bind("FireTime")] protected Text _fireTime;
+    [Bind("SecurityTime")] protected Text _securityTime;
+    [Bind("OpenButton")] protected UIButton _openButton;
+    [Bind("CloseButton")] protected UIButton _closeButton;
+
+    private Vector2 _originAnchoredPos;
+
+    private Coroutine _animateCoroutine = null;
+
+    protected override void Initialize()
+    {
+        _openButton.BindEvent(() => 
+        {
+            if (_animateCoroutine != null)
+                StopCoroutine(_animateCoroutine);
+            _animateCoroutine = StartCoroutine(AnimateUI(_originAnchoredPos, new Vector2(0, Rect.anchoredPosition.y)));
+        }, 
+        ClickType.Up);
+
+        _closeButton.BindEvent(() => 
+        {
+            if (_animateCoroutine != null)
+                StopCoroutine(_animateCoroutine);
+            _animateCoroutine = StartCoroutine(AnimateUI(new Vector2(0, Rect.anchoredPosition.y), _originAnchoredPos));
+        }, 
+        ClickType.Up);
+    }
+
+    public override void Open()
+    {
+        base.Open();
+
+        _originAnchoredPos = Rect.anchoredPosition;
+    }
+
+    void Update()
+    {
+        _fireTime.text = Managers.Event.Fire.RemainTime;
+        // _securityTime.text = Managers.Event.Security.RemainTime;
+    }
+
+    private IEnumerator AnimateUI(Vector3 from, Vector3 to)
+    {
+        float time = 0f;
+        float duration = 0.5f;
+
+        _openButton.gameObject.SetActive(!_openButton.gameObject.activeSelf);
+        _closeButton.gameObject.SetActive(!_closeButton.gameObject.activeSelf);
+
+        while (time < duration)
+        {
+            float t = time / duration;
+            float easedT = UIUtils.EaseInOutQuad(t);
+
+            Rect.anchoredPosition = Vector2.Lerp(from, to, easedT);
+
+            time += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
