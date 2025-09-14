75fead8dd2784fa65d4f15ca6458f33ed017da8d
namgyeongtae <gotsuebel@gmail.com>
2025-09-14 11:22:43 +0900

    Add Event Warning UI on Building

--- Assets/Scripts/UI/Normal/SceneUI.cs
+++ Assets/Scripts/UI/Normal/SceneUI.cs
@@ -5,20 +5,27 @@ using UnityEngine.UI;
 
 public class SceneUI : CanvasPanel
 {
+    [Header("UserInfo")]
     [Bind("SliderImage")] private Image _levelGaugeSlider;
-    [Bind("HomeButton")] private UIButton _homeButton;
     [Bind("Level")] private Text _levelText;
 
+    [Header("Commodity")]
     [Bind("WoodAmount")] private Text _woodAmount;
     [Bind("IronAmount")] private Text _ironAmount;
+    private Coroutine _woodCoroutine = null;
+    private Coroutine _ironCoroutine = null;
 
+    [Header("SideGroup")]
+    [Bind("HomeButton")] private UIButton _homeButton;
     [Bind("SettingButton")] private UIButton _settingButton;
     [Bind("QuestionButton")] private UIButton _questionButton;
     [Bind("MenuButton")] private UIButton _menuButton;
 
+    [Header("Build")]
     [Bind("UIBuildingSelection")]private UIBuildingSelection _buildingSelection;
 
     public UIBuildingSelection BuildingSelection => _buildingSelection;
+
 
     protected override void Initialize()
     {
@@ -99,12 +106,48 @@ public class SceneUI : CanvasPanel
 
     public void AddCommodity(Ingredient ingredient, float amount)
     {
-        StartCoroutine(CoAddCommodity(ingredient, amount));
+        switch (ingredient.Type)
+        {
+            case IngredientType.Wood:
+                if (_woodCoroutine != null)
+                {
+                    StopCoroutine(_woodCoroutine);
+                    _woodCoroutine = null;
+                }
+                _woodCoroutine = StartCoroutine(CoAddCommodity(ingredient, amount));
+                break;
+            case IngredientType.Iron:
+                if (_ironCoroutine != null)
+                {
+                    StopCoroutine(_ironCoroutine);
+                    _ironCoroutine = null;
+                }
+                _ironCoroutine = StartCoroutine(CoAddCommodity(ingredient, amount));
+                break;
+        }
     }
 
     public void SubCommodity(Ingredient ingredient, float amount)
     {
-        StartCoroutine(CoSubCommodity(ingredient, amount));
+        switch (ingredient.Type)
+        {
+            case IngredientType.Wood:
+                if (_woodCoroutine != null)
+                {
+                    StopCoroutine(_woodCoroutine);
+                    _woodCoroutine = null;
+                }
+                _woodCoroutine = StartCoroutine(CoSubCommodity(ingredient, amount));
+                break;
+            case IngredientType.Iron:
+                if (_ironCoroutine != null)
+                {
+                    StopCoroutine(_ironCoroutine);
+                    _ironCoroutine = null;
+                }
+                _ironCoroutine = StartCoroutine(CoSubCommodity(ingredient, amount));
+                break;
+        }
     }
 
     // 빌딩 선택 창 열기 혹은 닫기
