08f61287a1e9f2948d7d39b2efbcb3e6c4fc7190
namgyeongtae <gotsuebel@gmail.com>
2025-10-29 10:09:22 +0900

    Change Test Input Key

--- Assets/2.Scripts/Grid/PlacementSystem.cs
+++ Assets/2.Scripts/Grid/PlacementSystem.cs
@@ -20,7 +20,7 @@ public class PlacementSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.K))
         {
             if (_currentBuilding == null)
             {
