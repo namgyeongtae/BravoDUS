309a14c7a356644adb8c25f8ef74359fd553a6b9
namgyeongtae <gotsuebel@gmail.com>
2025-11-20 16:34:08 +0900

    Fix MainScene

--- Assets/2.Scripts/WorkForce.cs
+++ Assets/2.Scripts/WorkForce.cs
@@ -3,43 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public enum JobType
-{
-    None = -1,
-    WoodWorker,
-    IronWorker,
-    Doctor,
-    PoliceOfficer,
-    FireFighter
-}
-
-public enum HRState
-{
-    None,
-    Work,
-    Rest,
-    Tired,
-    Recovering,
-    Injured
-}
-
-[Serializable]
-public class WorkForceData
-{
-    public int Id;
-    public string Name;
-    public float Stamina;
-    public string JobType;
-    public string HRState;
-    public string Icon;
-}
-
-[Serializable]
-public class WorkForceDataWrapper : List<WorkForceData>
-{
-    
-}
-
 public class WorkForce
 {
     private WorkForceData _data;
